b65e074dc247fad7757765a95fb169edda1295dc
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-01-19 07:02:46 -1000

    added store for surfboards

---
+++ react-app/src/store/surfboard.js
@@ -0,0 +1,115 @@
+// constants
+const GET_LISTINGS = 'surfboards/GET_LISTINGS';
+const NEW_LISTING = 'surfboards/NEW_LISTING';
+const UPDATE_LISTING = 'surfboards/UPDATE_LISTING';
+const DELETE_LISTING = 'surfboards/DELETE_LISTING';
+
+const get_Listings = (listings) => ({
+  type: GET_LISTINGS,
+  listings
+});
+
+const new_Listing = (listing) => ({
+  type: NEW_LISTING,
+  listing
+});
+
+const update_Listing = (listing) => ({
+  type: UPDATE_LISTING,
+  listing
+});
+
+const delete_Listing = (id) => ({
+  type: DELETE_LISTING,
+  id
+});
+
+export const getListings = () => async (dispatch) => {
+    const res = await fetch('/api/surfboards/');
+    if (res.ok) {
+        const listings = await res.json();
+        dispatch(get_Listings(listings));
+        return listings;
+    };
+};
+
+export const newListing = (inputSurfboard) => async (dispatch) => {
+    const {description, image, size, location, ownerId} = inputSurfboard;
+    const res = await fetch('/api/surfboards/', {
+        method: 'POST',
+        body: JSON.stringify({
+            description, image, size, location, ownerId
+        })
+    });
+    if (res.ok) {
+        const listing = await res.json();
+        if (listing !== 'bad data') {
+            dispatch(new_Listing(listing));
+            return listing;
+        }
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) {
+            return data.errors
+        }
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+};
+
+export const updateListing = (inputListing) => async (dispatch) => {
+    const {id, description, image, size, location, ownerId} = inputListing;
+    const res = await fetch(`/api/surfboards/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            id, description, image, size, location, ownerId
+        })
+    });
+    if (res.ok) {
+        const listing = await res.json();
+        dispatch(update_Listing(listing));
+        return listing;
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+};
+
+export const deleteListing = (surfboardId) => async (dispatch) => {
+    const res = await fetch(`/api/surfboards/${surfboardId}`, {
+        method: 'DELETE'
+    });
+    if (res.ok) {
+        const msg = await res.json();
+        if (msg === 'Success') dispatch(delete_Listing(surfboardId));
+        return ['Could not delete.']
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+};
+
+const surfboardReducer = (state = {}, action) => {
+    let newState = {};
+    switch (action.type) {
+        case GET_LISTINGS:
+            action.listings.forEach(listing => {
+                newState[listing.id] = listing;
+            });
+            return newState;
+        case NEW_LISTING:
+            newState = Object.assign({}, state);
+            newState[action.listing.id] = action.listing;
+            return newState;
+        case UPDATE_LISTING:
+            newState = Object.assign({}, state);
+            newState[action.listing.id] = action.listing;
+            return newState;
+        case DELETE_LISTING:
+            newState = Object.assign({}, state);
+            delete newState[action.id];
+            return newState;
+        default:
+            return state;
+    }
+  };
+
+  export default surfboardReducer;

b7c7ecbc2fec1afd66ab70cf46a52f8ad75f9033
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-01-18 18:53:49 -1000

    renamed files

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.surboard_routes import surfboard_routes
+from .api.surfboard_routes import surfboard_routes
 
 from .seeds import seed_commands
 

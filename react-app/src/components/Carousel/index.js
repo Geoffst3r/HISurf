688583c51b80434b3ba6209f291f8602031b5097
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-03-12 12:05:18 -1000

    finished front page

--- react-app/src/components/Carousel/index.js
+++ react-app/src/components/Carousel/index.js
@@ -9,22 +9,24 @@ const Carousel = ({ listingsObj }) => {
     let listings, maxIndex;
     let carouselListings = [];
     let carouselLength;
-    if (mQuery > 1750) carouselLength = 11;
-    else if (mQuery > 1500) carouselLength = 8;
-    else if (mQuery > 1250) carouselLength = 7;
-    else if (mQuery > 1000) carouselLength = 5;
-    else if (mQuery > 750) carouselLength = 3;
+    if (mQuery > 2250) carouselLength = 9;
+    else if (mQuery > 2000) carouselLength = 8;
+    else if (mQuery > 1700) carouselLength = 7;
+    else if (mQuery > 1450) carouselLength = 6;
+    else if (mQuery > 1250) carouselLength = 5;
+    else if (mQuery > 1050) carouselLength = 4;
+    else if (mQuery > 800) carouselLength = 3;
     else if (mQuery > 600) carouselLength = 2;
     else carouselLength = 1;
 
     if (listingsObj) {
         listings = Object.values(listingsObj);
-        maxIndex = Math.ceil(listings.length/carouselLength - 1);
+        maxIndex = Math.ceil(listings.length / carouselLength - 1);
     };
 
     const getCarouselListings = () => {
         if (listings) {
-            for (let i = index*carouselLength; i < (index+1)*carouselLength; i++) {
+            for (let i = index * carouselLength; i < (index + 1) * carouselLength; i++) {
                 if (listings[i]) carouselListings.push(listings[i]);
             };
         };
@@ -51,14 +53,14 @@ const Carousel = ({ listingsObj }) => {
     if (carouselListings) {
         return (
             <div className='carousel-container'>
-                {index > 0 && <button onClick={leftArrow}><i className='fas fa-arrow-left fa-2x'/></button>}
+                {index > 0 && <button onClick={leftArrow}><i className='fas fa-arrow-left fa-2x' /></button>}
                 <ul className='listings-list'>
                     {carouselListings.map(listing => (
                         <li key={listing.id}>
                             <NavLink className='individual-item' to={`/surfboards/${listing.id}/`}>
                                 <div className='listing-img'>
-                                    {listing.image ? <img alt='' src={`${listing.image}`} />:
-                                    <img alt='' src='https://hi-surf-dev.s3.us-west-1.amazonaws.com/no-image.jpg' />}
+                                    {listing.image ? <img alt='' src={`${listing.image}`} /> :
+                                        <img alt='' src='https://hi-surf-dev.s3.us-west-1.amazonaws.com/no-image.jpg' />}
                                 </div>
                                 <div className='listing-location'>{listing.location}</div>
                                 <div className='listing-size'>{listing.size}' Board</div>
@@ -66,7 +68,7 @@ const Carousel = ({ listingsObj }) => {
                         </li>
                     ))}
                 </ul>
-                {index < maxIndex && <button onClick={rightArrow}><i className='fas fa-arrow-right fa-2x'/></button>}
+                {index < maxIndex && <button onClick={rightArrow}><i className='fas fa-arrow-right fa-2x' /></button>}
             </div>
         )
     } else {
--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,15 +1,15 @@
-import SelfImage from './SelfImage.jpg';
 import './footer.css';
 
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <p className='footer-title'>Built by Geoffrey Cox</p>
-            <img src={SelfImage} alt='' />
+            <p className='footer-title'>Built by</p>
             <div className='links'>
                 <a target='_blank' rel="noreferrer" href='https://github.com/Geoffst3r'><i className='fa fa-github fa-2x' /></a>
                 <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/geoffreyptcox/'><i className='fab fa-linkedin-in fa-2x' /></a>
+                <a target='_blank' rel="noreferrer" href='https://angel.co/u/geoffrey-cox-2'><i className='fab fa-angellist fa-2x' /></a>
+                <a target='_blank' rel="noreferrer" href='https://geoffst3r.github.io/'><i className='fa fa-folder-open fa-2x' /></a>
             </div>
         </div>
     )

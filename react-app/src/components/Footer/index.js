acb72fd1942432a77375d651e4324fa178da4125
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-03-12 20:25:07 -1000

    removed tag on footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -4,7 +4,6 @@ import './footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <p className='footer-title'>Built by</p>
             <div className='links'>
                 <a target='_blank' rel="noreferrer" href='https://github.com/Geoffst3r'><i className='fa fa-github fa-2x' /></a>
                 <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/geoffreyptcox/'><i className='fab fa-linkedin-in fa-2x' /></a>

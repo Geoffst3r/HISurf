fdf4be2e6d042d8f40edfd3900045a640f8cfc96
Geoffrey Cox <geoffrey.pt.cox@gmail.com>
2022-03-31 14:27:38 -1000

    revised button click to be more efficient depending on where it is clicked from

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
 import './NavBar.css'
 
 const NavBar = () => {
   const history = useHistory();
+  const params = useParams();
 
   const redirectHome = (e) => {
     e.preventDefault();
-    history.push('/surfboards/');
-    return window.location.reload(false);
+    if (params.surfboardId) return history.push('/surfboards/');
+    else return window.location.reload(false);
   };
 
   return (
